e891a977e25737c6386aa8d2ebf14023c6310438
Bruna de Paula <brulibra03@hotmail.com>
2023-12-23 20:42:45 -0300

    Interpolação Strings

--- Models/ModuleTwo/Course.cs
+++ Models/ModuleTwo/Course.cs
@@ -10,22 +10,11 @@ namespace dotnet_study.Models.ModuleTwo
         public string Name { get; set; }
         public List<People> Students { get; set; }
 
-        /*
-        Um método possuí algumas sessões características:
-        void -> não preciso, não espero nenhum retorno
-        NomeMétodo -> nome do método literalmente...
-        (Tipo parâmetro) -> Tipo, classe de onde vem o dado, variável que indica o aluno
-        */
         public void AddStudent(People student)
         {
             Students.Add(student);
         }
 
-        /*
-        Via de regra, todo étodo diferente de void,
-        espera um retorno, e não necessáriamente 
-        espera um argumento
-        */
         public int AmountOfStudentsEnrolled()
         {
             int amount = Students.Count;
@@ -35,17 +24,24 @@ namespace dotnet_study.Models.ModuleTwo
         public bool RemoveStudent(People student)
         {
 
-            //o proprio método remove retorna um bool
-            //true em caso de sucesso, false em caso de erro
             return Students.Remove(student);
 
         }
 
         public void ListStudents()
         {
-            foreach (People student in Students)
+
+            Console.WriteLine($"Alunos do Curso de {Name}");
+
+            for (int counter = 0; counter < Students.Count; counter++)
             {
-                Console.WriteLine(student.Fullname);
+                //nesse caso é preciso utilizar couter+1 ao invés de courter ++
+                //para não alterar o valor da variável contador 
+                //sim deixar a leitura mais amigável
+
+                string displayText = $"Nº {counter + 1} - {Students[counter].Fullname}";
+
+                Console.WriteLine(displayText);
             }
         }
     }
